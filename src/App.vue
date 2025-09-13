266222253614fcae7bd8baf6562c67af551a342a
Clem-Coding <clementine.blondeau@gmail.com>
2025-09-13 13:16:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ToDoInput from "./components/ToDoInput.vue";
+import TodoList from "./components/TodoList.vue";
+</script>
+
+<template>
+  <header>
+    <H1>TODO</H1>
+  </header>
+
+  <main></main>
+</template>
